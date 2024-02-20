2f85874f55eeba1fc2a16d91ec78c4c438082ac7
Garrett Woodward <gwoodward2@student.cvtc.edu>
2024-02-19 19:21:04 -0600

    Implimented input filtering for non-numerics

--- src/edu/cvtc/itsd/Main.java
+++ src/edu/cvtc/itsd/Main.java
@@ -53,7 +53,7 @@ public class Main {
     public void replace(FilterBypass fb, int offset, int lengthToDelete, String stringToAdd, AttributeSet attr)
         throws BadLocationException
     {
-      if (fb.getDocument() != null) {
+      if (stringToAdd.matches("[0-9]+")) {
         super.replace(fb, offset, lengthToDelete, stringToAdd, attr);
       }
       else {
